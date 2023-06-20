86b52b8eb459c685730c3196d8e11f3fbf8ed858
muhsinmuhsy <muhsinkoodali856@gmail.com>
2023-06-20 15:19:16 +0530

    /// added api product variants ///

--- apiapp/views.py
+++ apiapp/views.py
@@ -73,7 +73,7 @@ def product_detail_api(request, pk):
         
         data = [serializer.data,varients.data]
         
-        array = {data[0],data[1]}
+        array = {'product':data[0],'varients':data[1]}
         
         return Response(array)
 
