016ba7e0ca2c626aed9f625632743252f83e3f12
muhsinmuhsy <muhsinkoodali856@gmail.com>
2023-06-26 18:51:57 +0530

    /// added use api ///

--- adminapp/models.py
+++ adminapp/models.py
@@ -76,9 +76,8 @@ class Cart(models.Model):
     tax_dummy = models.CharField(max_length=100, null=True, blank=True)
     total = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     
-
     def __str__(self):
-        return self.user.name
+        return self.user.user_id
     
     def save(self, *args, **kwargs):
         if self.product_price.discount_price is not None:
